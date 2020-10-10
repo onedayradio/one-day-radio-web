92ff5f7e5561c1e3661afeb82071b3394ac3bee3
Pablo Gonzalez <pablohgm@gmail.com>
2020-10-10 10:50:08 -0600

    Add Search bar component (#38)
    Co-authored-by: pablo.gonzalez <pablo.gonzalez@udr.com>

---
+++ src/components/playlist/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { Input } from '@chakra-ui/core'
+
+interface SearchBarProps {
+  onSearch: any
+}
+
+export const SearchBar = React.memo(({ onSearch }: SearchBarProps) => {
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onSearch(searchText)
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [searchText, onSearch]);
+
+  return (
+    <Input
+      placeholder="Search on the playlist"
+      value={searchText}
+      onChange={(e:any) => setSearchText(e.target.value)}
+      mr={2}/>
+  )
+})
--- src/shared/graphql/queries/playlistQueries.ts
+++ src/shared/graphql/queries/playlistQueries.ts
@@ -4,8 +4,8 @@ import { pagination } from '../fragments/paginationFragments'
 import { playlistSongs } from '../fragments/playlistSongFragments'
 
 export const LOAD_PLAYLIST_SONGS = gql`
-  query loadPlaylistSongs($genreId: String) {
-    loadPlaylistSongs(genreId: $genreId) {
+  query loadPlaylistSongs($genreId: String, $searchText: String) {
+    loadPlaylistSongs(genreId: $genreId, searchText: $searchText) {
       ...PaginationFull
       ...PlaylistSongsFull
     }
