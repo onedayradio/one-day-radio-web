bbd7ce2bd30f8912134e2a26519e44bc68908bba
pablo.gonzalez <pablo.gonzalez@udr.com>
2020-10-12 07:49:22 -0600

    add song integration

--- src/components/song/SongCard.tsx
+++ src/components/song/SongCard.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Flex, Text, Avatar, Box, IconButton } from '@chakra-ui/core'
-import { Album, Song } from '../../shared'
+import { Album, ADD_SONG_TO_PLAYLIST, Song } from '../../shared'
+import { useMutation } from '@apollo/client'
 
 const DEFAULT_WIDTH = 300
 
 interface SongCardProps {
   song: Song
+  genreId: string
+}
+
+interface AddSongHandleProps {
+  addSongToPlaylist: any
+  setShared: any
+  id: string
+  name: string
+  uri: string
+  artists: string
+  genreId: string
 }
 
 const getImageUrl = (album: Album): string => {
@@ -13,9 +25,37 @@ const getImageUrl = (album: Album): string => {
   return imageUrl ? imageUrl.url : ''
 }
 
-const SongCardComp = ({ song }: SongCardProps) => {
-  const { name, artists, sharedBy, album, inPlaylist } = song
+const getFormattedDate = () => {
+  const currentDate = new Date()
+  return {
+    day: currentDate.getDate().toString(),
+    month: (currentDate.getMonth() + 1).toString(),
+    year: currentDate.getFullYear().toString(),
+  }
+}
+
+const addSongHandle = async ({addSongToPlaylist, setShared, id, name, uri, artists, genreId }: AddSongHandleProps) => {
+  const date = getFormattedDate()
+  const { data: { addSongToPlaylist: { sharedBy } } } = await addSongToPlaylist({
+    variables: {
+      song: {
+        id,
+        name,
+        uri,
+        artists,
+      },
+      genreId,
+      date,
+    }
+  })
+  setShared(sharedBy)
+}
+
+export const SongCard = React.memo(({ song, genreId }: SongCardProps) => {
+  const [ addSongToPlaylist, { loading } ] = useMutation(ADD_SONG_TO_PLAYLIST)
+  const { name, artists, album, id, uri, sharedBy } = song
   const imageUrl = getImageUrl(album)
+  const [shared, setShared] = useState(sharedBy)
   return (
   <Flex
     align='center'
@@ -30,13 +70,16 @@ const SongCardComp = ({ song }: SongCardProps) => {
       <Text>{ artists }</Text>
     </Box>
     <Box width='30%'>
-      <Text textAlign='center' visibility={inPlaylist ? 'initial' : 'hidden'}>Shared by { sharedBy }</Text>
+      <Text textAlign='center' visibility={shared ? 'initial' : 'hidden'}>Shared by { shared }</Text>
     </Box>
     <Box width='15%'>
-      <IconButton icon={inPlaylist ? 'check' : 'add'} aria-label="Add to playlist" isDisabled={inPlaylist} variant="outline"/>
+      <IconButton
+        icon={!!shared ? 'check' : 'add'}
+        onClick={() => addSongHandle({ addSongToPlaylist, setShared, id, name, uri, artists, genreId })}
+        aria-label="Add to playlist"
+        isDisabled={!!shared}
+        isLoading={ loading }
+        variant="outline"/>
     </Box>
   </Flex>
-)
-}
-
-export const SongCard = React.memo(SongCardComp)
+)})
---
+++ src/shared/graphql/mutations/playlistMutations.ts
@@ -0,0 +1,10 @@
+import gql from 'graphql-tag'
+
+export const ADD_SONG_TO_PLAYLIST = gql`
+  mutation ($genreId: String, $song: SongInput, $date: DateDataInput){
+    addSongToPlaylist(genreId: $genreId, song: $song, date: $date){
+      name
+      sharedBy
+    }
+  }
+`
